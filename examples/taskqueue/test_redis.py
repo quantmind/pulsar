6bf44c1109f6dbce6ff66e4bcb37fec01ccc1d5c
Luca Sbardella <luca.sbardella@gmail.com>
2013-09-03 08:00:15 +0100

    fixing taskqueue redis tests

--- examples/taskqueue/test_redis.py
+++ examples/taskqueue/test_redis.py
@@ -5,6 +5,7 @@ except ImportError:
     stdnet = None
     
 from pulsar.apps.test import unittest
+from pulsar.utils.internet import parse_connection_string, get_connection_string
  
 from . import test_local
 
@@ -14,7 +15,10 @@ class TestRedisTaskQueueOnThread(test_local.TestTaskQueueOnThread):
     
     @classmethod
     def task_backend(cls):
-        return cls.cfg.backend_server or 'redis://127.0.0.1:6379'
+        backend = cls.cfg.backend_server or 'redis://127.0.0.1'
+        sheme, host, params = parse_connection_string(backend, 6379)
+        params['name'] = cls.name()
+        return get_connection_string(sheme, host, params)
 
 
 @unittest.skipUnless(stdnet, 'Requires python-stdnet')
