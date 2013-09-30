9418db08100105d674dd4face833f3007d0ebe1e
quantmind <luca.sbardella@citi.com>
2013-09-30 09:41:53 +0100

    use python-coveralls

--- travisrun.py
+++ travisrun.py
@@ -1,21 +1,23 @@
 import sys
 
 import coverage
-from coveralls import cli
+import coveralls
 
 
 if __name__ == '__main__':
     if sys.version_info > (3, 3):
         cov = coverage.coverage(data_suffix='travisrun')
         cov.start()
-        from runtests import run
+        from runtests import run, Path
+        import pulsar
         run(coverage=True, profile=True)
         cov.stop()
         cov.save()
         cov = coverage.coverage(data_suffix=True)
         cov.combine()
         cov.save()
-        cli.main(argv=[])
+        path = Path(pulsar.__file__)
+        coveralls.wear(argv=['--base_dir', path.parent.parent])
     else:
         from runtests import run
         run()
