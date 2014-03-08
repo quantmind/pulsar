1eaf7d69c48cc59e8b78007cd90751b98e4e48f7
Luca Sbardella <luca.sbardella@gmail.com>
2014-03-08 22:14:55 +0000

    removed some code from coverage

--- tests/stores/mongodb.py
+++ tests/stores/mongodb.py
@@ -21,14 +21,13 @@ class d:
     def test_aggregate(self):
         db = yield self.store.database()
         test = db.test1
-        id1 = yield test.insert({"src":"Twitter", "content":"bla bla"},
-                                   safe=True)
-        id2 = yield test.insert({"src":"Twitter", "content":"more data"},
+        id1 = yield test.insert({"src": "Twitter", "content": "bla bla"},
+                                safe=True)
+        id2 = yield test.insert({"src": "Twitter", "content": "more data"},
                                 safe=True)
 
         # Read more about the aggregation pipeline in MongoDB's docs
-        pipeline = [
-            {'$group': {'_id':'$src', 'content_list': {'$push': '$content'} } }
-            ]
+        pipeline = [{'$group': {'_id': '$src',
+                                'content_list': {'$push': '$content'}}}]
         result = yield test.aggregate(pipeline)
         self.assertTrue(result)
