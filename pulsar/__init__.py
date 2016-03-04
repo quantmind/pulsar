c83e1786a7ef7a0ef9870f59e19f9b4099673d5d
Luca Sbardella <luca@quantmind.com>
2016-03-04 22:19:07 +0000

    Hindi pulsar, need double check #release-note

--- pulsar/__init__.py
+++ pulsar/__init__.py
@@ -37,6 +37,8 @@ ASYNC_TIMEOUT = None
 SERVER_NAME = 'pulsar'
 JAPANESE = b'\xe3\x83\x91\xe3\x83\xab\xe3\x82\xb5\xe3\x83\xbc'.decode('utf-8')
 CHINESE = b'\xe8\x84\x89\xe5\x86\xb2\xe6\x98\x9f'.decode('utf-8')
+HINDI = (b'\xe0\xa4\xaa\xe0\xa4\xb2\xe0\xa5\x8d'
+         b'\xe0\xa4\xb8\xe0\xa4\xb0').decode('utf-8')
 SERVER_SOFTWARE = "{0}/{1}".format(SERVER_NAME, version)
 
 if os.environ.get('pulsar_setup_running') != 'yes':
