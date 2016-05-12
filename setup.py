1f72b82b57f91e05c571320552e4c43485f369e3
Luca Sbardella <luca@quantmind.com>
2016-05-12 08:47:10 +0100

    3.6 is not out yet

--- setup.py
+++ setup.py
@@ -81,7 +81,6 @@ meta = dict(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Topic :: Internet',
         'Topic :: Utilities',
         'Topic :: System :: Distributed Computing',
