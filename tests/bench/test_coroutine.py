a042d456ca5aba3feef813676409b8958b119177
Luca Sbardella <luca@quantmind.com>
2016-09-29 22:36:35 +0100

    added bench test suite to CI

--- tests/bench/test_coroutine.py
+++ tests/bench/test_coroutine.py
@@ -1,52 +1,42 @@
 import unittest
-import asyncio
 
-from pulsar import ensure_future, new_event_loop, Future
+from pulsar import create_future, get_event_loop
 
 
 DELAY = 0
 
 
 def async_func(loop, value):
-    p = Future(loop=loop)
+    p = create_future(loop)
     loop.call_later(DELAY, p.set_result, value)
     return p
 
 
-@asyncio.coroutine
-def sub_sub(loop, num):
-    a = yield from async_func(loop, num)
-    b = yield from async_func(loop, num)
+async def sub_sub(loop, num):
+    a = await async_func(loop, num)
+    b = await async_func(loop, num)
     return a+b
 
 
-@asyncio.coroutine
-def sub(loop, num):
-    a = yield from async_func(loop, num)
-    b = yield from async_func(loop, num)
-    c = yield from sub_sub(loop, num)
-    return a+b+c
+async def sub(loop, num):
+    return (
+        await async_func(loop, num) +
+        await async_func(loop, num) +
+        await sub_sub(loop, num)
+    )
 
-
-@asyncio.coroutine
-def main(loop, num):
-    a = yield from async_func(loop, num)
-    b = yield from sub(loop, num)
-    c = yield from sub(loop, num)
+async def main(num, loop=None):
+    loop = loop or get_event_loop()
+    a = await async_func(loop, num)
+    b = await sub(loop, num)
+    c = await sub(loop, num)
     return a+b+c
 
 
 class TestCoroutine(unittest.TestCase):
     __benchmark__ = True
-    __number__ = 100
+    __number__ = 1000
 
-    def setUp(self):
-        self.loop = new_event_loop()
-
-    def test_coroutine(self):
-        future = ensure_future(main(self.loop, 1), loop=self.loop)
-        self.loop.run_until_complete(future)
-        self.assertEqual(future.result(), 9)
-
-    def getTime(self, dt):
-        return dt - 9*DELAY
+    async def test_coroutine(self):
+        result = await main(1)
+        self.assertEqual(result, 9)
