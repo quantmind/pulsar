3ec87a1fb58b9d62a06bcf6a65358e4f230c6b43
Luca Sbardella <luca@quantmind.com>
2016-10-04 19:11:00 +0100

    set system path first

--- pulsar/async/_subprocess.py
+++ pulsar/async/_subprocess.py
@@ -5,11 +5,12 @@ if __name__ == '__main__':
     from multiprocessing import current_process
     from multiprocessing.spawn import import_main_path
 
-    from pulsar.async.concurrency import run_actor
-
     data = pickle.load(sys.stdin.buffer)
     current_process().authkey = data['authkey']
     sys.path = data['path']
     import_main_path(data['main'])
     impl = pickle.loads(data['impl'])
+
+    from pulsar.async.concurrency import run_actor
+
     run_actor(impl)
