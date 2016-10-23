d2f7bf38a907b01570c0ff92edb85c9ba3a747d0
Luca Sbardella <luca@quantmind.com>
2016-10-23 18:05:09 +0100

    regression

--- pulsar/apps/data/pulsards/startds.py
+++ pulsar/apps/data/pulsards/startds.py
@@ -39,7 +39,7 @@ async def start_store(app, url, workers=0, **kw):
             cfg = await send('arbiter', 'run', start_pulsar_ds,
                              host, workers)
             store._host = cfg.addresses[0]
-            dns = store._buildurl()
+            dns = store.buildurl()
             store = create_store(dns, **kw)
     app.cfg.set('data_store', store.dns)
 
