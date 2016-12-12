98d6c0b310f75a5aaf4c50c5282b2700b058b238
Luca Sbardella <luca@quantmind.com>
2016-12-12 09:12:41 +0000

    platform.type

--- tests/async/test_actor_subprocess.py
+++ tests/async/test_actor_subprocess.py
@@ -7,6 +7,6 @@ from tests.async.actor import ActorTest
 
 
 @dont_run_with_thread
-@skipUnless(platform != 'win', 'Requires posix OS')
+@skipUnless(platform.type != 'win', 'Requires posix OS')
 class TestActorMultiProcess(ActorTest, unittest.TestCase):
     concurrency = 'subprocess'
