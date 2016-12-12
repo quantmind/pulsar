1aaa16b1322407cc4e4d60970b14c4e156669880
Luca Sbardella <luca@quantmind.com>
2016-12-12 10:06:04 +0000

    no tls testing either

--- tests/http/test_tls.py
+++ tests/http/test_tls.py
@@ -1,6 +1,7 @@
 import os
 
 from pulsar.apps.http import SSLError, HttpClient
+from pulsar.utils.system import platform
 
 from tests.http import base
 
@@ -8,14 +9,16 @@ from tests.http import base
 crt = os.path.join(os.path.dirname(__file__), 'ca_bundle')
 
 
-class TestTlsHttpClient(base.TestHttpClient):
-    with_tls = True
+if platform.type != 'win':
 
-    async def test_verify(self):
-        c = HttpClient()
-        await self.wait.assertRaises(SSLError, c.get, self.httpbin())
-        response = await c.get(self.httpbin(), verify=False)
-        self.assertEqual(response.status_code, 200)
-        response = await c.get(self.httpbin(), verify=crt)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.request.verify, crt)
+    class TestTlsHttpClient(base.TestHttpClient):
+        with_tls = True
+
+        async def test_verify(self):
+            c = HttpClient()
+            await self.wait.assertRaises(SSLError, c.get, self.httpbin())
+            response = await c.get(self.httpbin(), verify=False)
+            self.assertEqual(response.status_code, 200)
+            response = await c.get(self.httpbin(), verify=crt)
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.request.verify, crt)
