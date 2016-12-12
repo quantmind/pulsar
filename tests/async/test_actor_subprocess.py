600734e2fe8db42f2676ffcde984c005a96e22a7
Luca Sbardella <luca@quantmind.com>
2016-12-12 09:09:35 +0000

    subprocess in windows don't work with select

--- tests/async/test_actor_subprocess.py
+++ tests/async/test_actor_subprocess.py
@@ -6,7 +6,7 @@ from pulsar.utils.system import platform
 from tests.async.actor import ActorTest
 
 
-@skipUnless(platform != 'win', 'Requires posix OS')
 @dont_run_with_thread
+@skipUnless(platform != 'win', 'Requires posix OS')
 class TestActorMultiProcess(ActorTest, unittest.TestCase):
     concurrency = 'subprocess'
