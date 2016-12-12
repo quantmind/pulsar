56f5755c979a1db5e04349db8e86d500c3c8bda9
Luca Sbardella <luca@quantmind.com>
2016-12-12 08:51:01 +0000

    dont run subprocess tests in windows

--- tests/async/test_actor_subprocess.py
+++ tests/async/test_actor_subprocess.py
@@ -1,10 +1,12 @@
 import unittest
 
-from pulsar.apps.test import dont_run_with_thread
+from pulsar.apps.test import dont_run_with_thread, skipUnless
+from pulsar.utils.system import platform
 
 from tests.async.actor import ActorTest
 
 
+@skipUnless(platform != 'win', 'Requires posix OS')
 @dont_run_with_thread
 class TestActorMultiProcess(ActorTest, unittest.TestCase):
     concurrency = 'subprocess'
