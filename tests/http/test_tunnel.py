49ff6d0f1793fb0ac386f35d0a57b575da023e63
Luca Sbardella <luca@quantmind.com>
2016-12-12 09:59:36 +0000

    remove tunnel tests - running out of options

--- tests/http/test_tunnel.py
+++ tests/http/test_tunnel.py
@@ -1,10 +1,10 @@
-from pulsar.apps.test import skipUnless
 from pulsar.utils.system import platform
 
 from tests.http import base, req
 
 
-@skipUnless(platform.type != 'win', 'Hangs on appveyor - need fixing')
-class TestTlsHttpClientWithProxy(req.TestRequest, base.TestHttpClient):
-    with_proxy = True
-    with_tls = True
+if platform.type != 'win':
+
+    class TestTlsHttpClientWithProxy(req.TestRequest, base.TestHttpClient):
+        with_proxy = True
+        with_tls = True
