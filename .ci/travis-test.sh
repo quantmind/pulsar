a76e027278915ae10539c0b6d8fabdf59fac5e94
Luca Sbardella <luca@quantmind.com>
2017-02-10 10:25:15 +0000

    ci scripts fixes

--- .ci/travis-test.sh
+++ .ci/travis-test.sh
@@ -10,15 +10,12 @@ fi
 
 make clean && make coverage
 
-if [[ $TRAVIS_PYTHON_VERSION == $COVERAGE ]];
-    then python setup.py test --coveralls;
+if [[ $COVERALLS == "yes" ]]; then
+    python setup.py test --coveralls;
+    cd docs && make spelling;
 fi
 
 python -W ignore setup.py test -q --io uv
 python -W ignore setup.py test --http-py-parser -q
 python setup.py bench
-
 # - python setup.py bench --io uv
-if [[ $TRAVIS_PYTHON_VERSION == $COVERAGE ]];
-    then cd docs && make spelling;
-fi
